6309783c18c76b3c76ba9a7ef6cb702cf423ce23
{{w84d8}} <{{rakonline99@gmail.com}}>
2023-11-26 09:01:44 +0200

    Coca-Cola Light

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Address myAddress = new Address();
+
+        myAddress.Index = 12345;
+        myAddress.Country = "Ukraine";
+        myAddress.City = "Kyiv";
+        myAddress.Street = "Pushkyna street";
+        myAddress.House = "42";
+        myAddress.Apartment = "18";
+
+        Console.WriteLine($"Index: {myAddress.Index}");
+        Console.WriteLine($"Country: {myAddress.Country}");
+        Console.WriteLine($"City: {myAddress.City}");
+        Console.WriteLine($"Street: {myAddress.Street}");
+        Console.WriteLine($"House: {myAddress.House}");
+        Console.WriteLine($"Apartment: {myAddress.Apartment}");
+    }
+}
---
+++ Task 1/Task 1/address.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Address
+{
+    private int index;
+    private string country;
+    private string city;
+    private string street;
+    private string house;
+    private string apartment;
+
+    public int Index
+    {
+        get { return index; }
+        set { index = value; }
+    }
+
+    public string Country
+    {
+        get { return country; }
+        set { country = value; }
+    }
+
+    public string City
+    {
+        get { return city; }
+        set { city = value; }
+    }
+
+    public string Street
+    {
+        get { return street; }
+        set { street = value; }
+    }
+
+    public string House
+    {
+        get { return house; }
+        set { house = value; }
+    }
+
+    public string Apartment
+    {
+        get { return apartment; }
+        set { apartment = value; }
+    }
+}
