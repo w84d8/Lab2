d6d22d0f1b34b544379c4c13cff74344b7b28c1f
{{w84d8}} <{{rakonline99@gmail.com}}>
2023-11-26 09:15:19 +0200

    Coca-Cola Light

---
+++ Task 2/Converter.cs
@@ -0,0 +1,43 @@
+﻿public class Converter
+{
+    private double usdRate;
+    private double eurRate;
+    private double plnRate;
+
+    public Converter(double usd, double eur, double pln)
+    {
+        usdRate = usd;
+        eurRate = eur;
+        plnRate = pln;
+    }
+
+    public double ConvertToUsd(double uah)
+    {
+        return uah / usdRate;
+    }
+
+    public double ConvertToEur(double uah)
+    {
+        return uah / eurRate;
+    }
+
+    public double ConvertToPln(double uah)
+    {
+        return uah / plnRate;
+    }
+
+    public double ConvertFromUsd(double usd)
+    {
+        return usd * usdRate;
+    }
+
+    public double ConvertFromEur(double eur)
+    {
+        return eur * eurRate;
+    }
+
+    public double ConvertFromPln(double pln)
+    {
+        return pln * plnRate;
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Converter converter = new Converter(28.0, 33.5, 7.8);
+
+        double amountInUah = 1000.0;
+        double amountInUsd = converter.ConvertToUsd(amountInUah);
+        double amountInEur = converter.ConvertToEur(amountInUah);
+        double amountInPln = converter.ConvertToPln(amountInUah);
+
+        Console.WriteLine($"{amountInUah} uah = {amountInUsd} usd");
+        Console.WriteLine($"{amountInUah} uah = {amountInEur} eur");
+        Console.WriteLine($"{amountInUah} uah = {amountInPln} pln");
+
+        double amountInUsdToUah = converter.ConvertFromUsd(50.0);
+        double amountInEurToUah = converter.ConvertFromEur(40.0);
+        double amountInPlnToUah = converter.ConvertFromPln(30.0);
+
+        Console.WriteLine($"{50.0} usd = {amountInUsdToUah} uah");
+        Console.WriteLine($"{40.0} eur = {amountInEurToUah} uah");
+        Console.WriteLine($"{30.0} pln = {amountInPlnToUah} uah");
+    }
+}
